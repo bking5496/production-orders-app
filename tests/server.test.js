44801dc11dab28d09cae6936b649e003c25bcc25
bking5496 <bking5496@gmail.com>
2025-08-12 21:50:01 +0200

    Auto update on Tue Aug 12 21:50:01 SAST 2025

--- tests/server.test.js
+++
@@ -1,9 +0,0 @@
-const request = require('supertest');
-const server = require('../server');
-
-describe('Server', () => {
-  it('should be running', async () => {
-    const response = await request(server).get('/api/health');
-    expect(response.status).toBe(200);
-  });
-});
